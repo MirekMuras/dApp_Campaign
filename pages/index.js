e94a06aec5fd5b7a34397c04c70a93e2183d159e
Miroslav Muras <mirekmuras@gmail.com>
2018-09-14 21:40:13 -0400

    Next.js instaled

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+//@dev: Configure web3 with provider from MetaMask
+//@dev: Tell web3 that a deployed copy of the 'campaignFactory' exist
+//@dev: Use Factory instance to retreive a list of deployed campaigns
+//@dev: use React to show something about each campaign
+
+export default () => {
+    return <h1>This is the campaign list page.</h1>
+};
